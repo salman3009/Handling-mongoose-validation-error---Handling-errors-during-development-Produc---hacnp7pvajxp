5699f7757f135abcced3177a0cc8a56f5a846db9
salman3009 <salman444g@gmail.com>
2023-07-23 11:45:39 +0530

    code

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,6 +2,20 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+   name:{
+    type:String,
+    required:[true,'Name is required']
+   },
+   email:{
+    type:String,
+    unique:true,
+    validate:{
+      validator:function(value){
+         return /^\S+@\S+\.\S+$/.test(value);
+      },
+      message:'Invalid email format'
+    },
+   },
   //Write a schema for name and email here
   // name:type string and required true
   //email: type String and required true, also write a custom validator
